8c777dfe15e38f38f8ea9851ebfef572a57253b9
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-17 12:24:43 -0700

    refactor(command): pass the command to be executed by watcher

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const path = require('path');
 
 const target = path.join(process.cwd(), process.argv[2]);
 const option = process.argv[3];
+// ToDo: create rsync command
+// https://www.digitalocean.com/community/tutorials/how-to-use-rsync-to-sync-local-and-remote-directories-on-a-vps
+const command = process.argv[4] || '';
 
 if (option === 'start') {
-  exec('nohup node ./src/watcher.js ' + target + '> /dev/null 2>&1 &', function(error, stdout, stderr) {
+  exec('nohup node ./src/watcher.js ' + target + ' ' + command + '> /dev/null 2>&1 &', function(error, stdout, stderr) {
     console.log('watching ' + target + ' for changes');
   });
 }
--- src/watcher.js
+++ src/watcher.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
+const exec = require('child_process').exec;
 
-function watch(dir) {
+function watch(dir, command) {
   fs.watch(dir, {
     persistent: true,
     recursive: true
   },
   (event, filename) => {
     if (filename) {
-      console.log(`filename provided: ${filename}`);
+      exec(command);
     }
   });
 }
