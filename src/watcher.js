893bc3c46f3586b598553574f771ca1eb8b2ba59
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-16 19:35:29 -0700

    fix(watch): directory recursively for change

--- src/watcher.js
+++ src/watcher.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
 function watch(dir) {
-  fs.watch(dir, (event, filename) => {
+  fs.watch(dir, {
+    persistent: true,
+    recursive: true
+  },
+  (event, filename) => {
     if (filename) {
       console.log(`filename provided: ${filename}`);
     }
