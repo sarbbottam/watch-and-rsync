53afb50d6239dc7014779348d8af0f81e383d197
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-16 17:04:53 -0700

    docs(comments): annotated code with references
    Gist & StackOverflow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// nohup.js - https://gist.github.com/supersha/6913695
 const exec = require('child_process').exec;
 const path = require('path');
 
@@ -11,6 +12,8 @@ if (option === 'start') {
 }
 
 if (option === 'stop') {
+  // http://stackoverflow.com/questions/31570240/nodejs-get-process-id-from-within-shell-script-exec
+  // http://stackoverflow.com/questions/12941083/get-the-output-of-a-shell-command-in-node-js
   exec('ps -ef | grep ' + target + ' | grep -v grep | cut -d" " -f2', function(error, stdout, stderr) {
     const pid = stdout.split('\n')[0];
     exec('kill -9 ' + pid);
