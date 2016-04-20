e00b947dcbc155b5e62dcb72eefa70b2d0c8e5da
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-20 13:39:02 -0700

    chore(lint): using xo for linting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // nohup.js - https://gist.github.com/supersha/6913695
-const tildify = require('tildify');
-const username = require('username');
-
 const exec = require('child_process').exec;
 const path = require('path');
+const tildify = require('tildify');
+// const username = require('username');
 
 const options = {
   option: ['o'],
@@ -13,32 +12,31 @@ const options = {
   user: ['u']
 };
 
-var argv = require('yargs')
+const argv = require('yargs')
   .demand(['o', 's', 't'])
   .strict()
   .alias(options)
-  .argv
+  .argv;
 
-const option = argv.o;
 const source = path.join(process.cwd(), argv.s);
 const target = tildify(path.join(process.cwd(), argv.t));
-const user = argv.u || username.sync();
-const hostname = argv.h
+// const user = argv.u || username.sync();
+// const hostname = argv.h
 
 // https://www.digitalocean.com/community/tutorials/how-to-use-rsync-to-sync-local-and-remote-directories-on-a-vps
-const command = `'rsync -a --delete ${tildify(source)}/ ${target}'`
+const command = `'rsync -a --delete ${tildify(source)}/ ${target}'`;
 
 if (argv.o === 'start') {
-  exec(`nohup ./node_modules/.bin/watch-and-exec -d=${source} -c=${command} > /dev/null 2>&1 &`, (error, stdout, stderr) => {
-    console.log('watching ' + source + ' for changes');
+  exec(`nohup ./node_modules/.bin/watch-and-exec -d=${source} -c=${command} > /dev/null 2>&1 &`, () => {
+    console.log(`watching ${source} for changes`);
   });
 }
 
 if (argv.o === 'stop') {
   // http://stackoverflow.com/questions/31570240/nodejs-get-process-id-from-within-shell-script-exec
   // http://stackoverflow.com/questions/12941083/get-the-output-of-a-shell-command-in-node-js
-  exec('ps -ef | grep ' + source + ' | grep -v grep | awk \'{print $2}\'', (error, stdout, stderr) => {
+  exec(`ps -ef | grep ${source} | grep -v grep | awk '{print $2}'`, (error, stdout) => {
     const pid = stdout.split('\n')[0];
-    exec('kill -9 ' + pid);
+    exec(`kill -9 ${pid}`);
   });
 }
