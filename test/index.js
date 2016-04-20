ae317da1489984baf86a537220ab9e2d5be6e75e
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-20 14:48:15 -0700

    fix(test): use absolute file path

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
 const assert = require('assert');
 
 const fs = require('fs');
+const path = require('path');
 const exec = require('child_process').exec;
 
-exec(`mkdir -p ./test/fixtures/{source,target}`);
+exec(`mkdir -p ./test/fixtures/source`);
+exec(`mkdir -p ./test/fixtures/target`);
 
 exec(`./src/index.js -o=start -s=./test/fixtures/source -t=./test/fixtures/target`, error => {
   assert.equal(error, null);
@@ -12,7 +14,7 @@ exec(`./src/index.js -o=start -s=./test/fixtures/source -t=./test/fixtures/targe
     exec(`touch ./test/fixtures/source/foo`);
     setTimeout(() => {
       exec(`ps -ef | grep watch-and-exec | grep -v grep | awk '{print $2}' | xargs kill -9`, () => {
-        const error = fs.accessSync('./test/fixtures/source/foo', fs.F_OK);
+        const error = fs.accessSync(path.join(process.cwd(), './test/fixtures/target/foo'), fs.F_OK);
         assert.equal(error, null);
         console.log('All the test have passed successfully');
         exec(`rm -rf ./test/fixtures`);
