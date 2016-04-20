32d393248045621c3cfa3f98bb08c040d9b4ae5b
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-20 15:59:11 -0700

    feat(rsync): remote host over ssh

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+'use strict';
+
 // nohup.js - https://gist.github.com/supersha/6913695
 const exec = require('child_process').exec;
 const path = require('path');
 const tildify = require('tildify');
-// const username = require('username');
+const username = require('username');
 
 const options = {
   option: ['o'],
@@ -15,22 +17,37 @@ const options = {
 };
 
 const argv = require('yargs')
-  .demand(['o', 's', 't'])
+  .demand(['o', 's'])
   .strict()
   .alias(options)
   .argv;
 
 const source = path.join(process.cwd(), argv.s);
-const target = tildify(path.join(process.cwd(), argv.t));
-// const user = argv.u || username.sync();
-// const hostname = argv.h
+argv.t = argv.t || argv.s;
+let target = tildify(path.join(process.cwd(), argv.t));
+let ssh = '';
+const user = argv.u || username.sync();
+const hostname = argv.h;
+// should be generated
+const exclude = '--exclude node_modules';
+
+if (hostname) {
+  target = `${user}@${hostname}:${target}`;
+  ssh = 'ssh';
+}
 
 // https://www.digitalocean.com/community/tutorials/how-to-use-rsync-to-sync-local-and-remote-directories-on-a-vps
-const command = `'rsync -a --delete ${tildify(source)}/ ${target}'`;
+const command = `'rsync -azOte ${ssh} --delete ${exclude} ${tildify(source)}/ ${target}'`;
+
+const watcher = path.join(__dirname, '../node_modules/.bin/watch-and-exec');
+console.log(watcher);
+console.log(command);
 
 if (argv.o === 'start') {
-  exec(`nohup ./node_modules/.bin/watch-and-exec -d=${source} -c=${command} > /dev/null 2>&1 &`, () => {
+  exec(`nohup ${watcher} -d=${source} -c=${command} > /dev/null 2>&1 &`, () => {
     console.log(`watching ${source} for changes`);
+    console.log(`via ${watcher}`);
+    console.log(`to excute ${command}`);
   });
 }
 
